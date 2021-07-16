6f44067fb911e745800b56c9547fd5edfd05bcd4
Matthias Zach <zach@math.uni-hannover.de>
2021-07-16 19:14:12 +0200

    Fundamentals for rational representations

---
+++ AlgebraicGroups.jl
@@ -0,0 +1,107 @@
+include( "Multiindices.jl" )
+
+module Representations
+
+using Oscar
+using Main.Multiindices
+using Main.Misc
+
+import AbstractAlgebra.Field
+import AbstractAlgebra.Generic.MatSpaceElem
+
+export AlgGroupRep, subgroup, representation_on_ext_power, null_cone
+
+
+#abstract type AlgebraicGroupElem end
+
+#abstract type Field = Union{ Nemo.FlintRationalField } end
+
+mutable struct AlgGroupRep
+  R::MPolyRing 		# The underlying polynomial ring
+  I::MPolyIdeal		# The ideal describing the closed subgroup
+  A::MatSpaceElem	# The matrix for the representation
+
+  # default constructor for GL(n;QQ)
+  function AlgGroupRep( n::Int )
+    #R_t,t = PolynomialRing(QQ,"t")
+    R, A,t = PolynomialRing(QQ, "a" => (1:n, 1:n), "t" => (1:1))
+    A = matrix(A)
+    t = t[1]
+    I = ideal(R, [1-det(A)*t] )
+    return new( R, I, A )
+  end
+
+  function AlgGroupRep( R::MPolyRing, I::MPolyIdeal, A::MatSpaceElem )
+    return new( R,I,A )
+  end
+
+end
+
+function subgroup( G::AlgGroupRep, I::MPolyIdeal )
+  return AlgGroupRep( G.R, G.I + I, G.A )
+end
+
+mutable struct AlgGroupElem
+  parent::AlgGroupRep
+  coord::Matrix{fmpq}
+  function AlgGroupElem( G::AlgGroupRep, A::Matrix{fmpq} ) 
+    # sanity check 
+    if !(nrows(A) == nrows( G.A ) && ncols(A) == ncols( G.A ))
+      println( "matrix sizes are incompatible" )
+      return
+    end
+    return new(G,A)
+  end
+end
+
+
+function mult( X::AlgGroupElem, Y::AlgGroupElem ) 
+  if ncols(X) != nrows(Y)
+    error( "Incompatible sizes of matrices" )
+    return
+  end
+  if X.parent != Y.parent 
+    error( "Elements do not live in the same group" )
+    return
+  end
+  return Z = AlgGroupElem( X.parent, X.A*Y.A )
+end
+
+function representation_on_sym_power( G::AlgGroupRep, k::Int )
+  # return the representation induced on the k-th symmetric power 
+  # of the vector space V on which G is defined.
+end
+
+function representation_on_ext_power( G::AlgGroupRep, p::Int )
+  # return the representation on the p-th exterior power of the vector 
+  # space V on which G is defined. 
+
+  n = ncols( G.A )
+  N = binomial( n, p )
+  M = MatrixSpace( G.R, N, N )
+  B = M(0)
+  for i in OrderedMultiindex( n, p )
+    for j in OrderedMultiindex( n, p )
+      B[ linear_index( i ), linear_index(j) ] = index_signature(i)*index_signature(j)*det( G.A[i.index,j.index] )
+    end
+  end
+
+  return AlgGroupRep( G.R, G.I, B )
+end
+
+function null_cone( H::AlgGroupRep )
+  # Assemble the ring for the homomorphism needed for the computation
+  n = ncols( H.A )
+  M, phi, v = add_variables( H.R, vcat( [ "x$i" for i in (1:n) ], [ "y$i" for i in (1:n) ]))
+  A = matrix(phi.(H.A))
+  h = matrix(v[n+1:2*n]) - A*matrix(v[1:n])
+  I = ideal(M,vec(collect(h))) + ideal( M, [ phi(f) for f in gens(H.I) ] )
+  b = eliminate( I, gens(M)[1:length(gens(H.R))] )
+  R, x = PolynomialRing( base_ring(M), [ "x$i" for i in (1:n) ] ) 
+  psi = AlgebraHomomorphism( M, R, vcat( [ zero(R) for i in (1:length(gens(M))-n)], x ))
+  J = [ psi(f) for f in gens(b) ]
+  return R, J
+end
+  
+
+end
